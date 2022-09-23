7d6e3790051f36480836a494a8dd7385fe395553
张晋铭 <1574768045@qq.com>
2022-09-23 16:31:42 +0800

    StackAndQueueCode:singleStack(no duplicate values)

--- algorithms/src/com/sameen/study/leetcode/StackAndQueueCode.java
+++ algorithms/src/com/sameen/study/leetcode/StackAndQueueCode.java
@@ -1,5 +1,6 @@
 package com.sameen.study.leetcode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -12,7 +13,7 @@ import java.util.List;
 public class StackAndQueueCode {
 
     public static void main(String[] args) {
-
+        List<int[]> res = singleStack(new int[]{3, 4, 1, 5, 6, 2, 7});
     }
 
     /**
@@ -44,11 +45,31 @@ public class StackAndQueueCode {
 
     /**
      * 给定一个不含有重复值的数组arr，找到每一个i位置左边和右边离i位置最近且值比arr[i]小的位置。返回所有位置相应的信息。
+     *
      * @param arr
      * @return
      */
     public static List<int[]> singleStack(int[] arr) {
-
+        //example: 3 4 1 5 6 2 7
+        Deque<Integer> leftStack = new ArrayDeque<>();
+        Deque<Integer> rightStack = new ArrayDeque<>();
+        List<int[]> res = new ArrayList<>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            res.add(new int[]{-1, -1});
+        }
+        for (int i = 0; i < arr.length; i++) {
+            while (!leftStack.isEmpty() && arr[i] < arr[leftStack.peekLast()]) {
+                int[] tmp = res.get(leftStack.pollLast());
+                tmp[1] = i;
+            }
+            while (!rightStack.isEmpty() && arr[arr.length - i - 1] < arr[rightStack.peekLast()]) {
+                int[] tmp = res.get(rightStack.pollLast());
+                tmp[0] = arr.length - i - 1;
+            }
+            leftStack.addLast(i);
+            rightStack.addLast(arr.length - i - 1);
+        }
+        return res;
     }
 
 }
