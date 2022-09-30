7c9941d198e5e531582b0e951cd6b0c3b29ae703
张晋铭 <1574768045@qq.com>
2022-09-30 17:35:04 +0800

    add file

---
+++ algorithms/src/com/sameen/study/leetcode/topics/Array.java
@@ -0,0 +1,23 @@
+package com.sameen.study.leetcode.topics;
+
+/**
+ * @author: zhangjinming on 2022/9/30
+ * @description:
+ */
+public class Array {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 1 2 3 4 1 2 3 4
+     * @param n
+     * @param k
+     * @return
+     */
+    public int findTheWinner(int n, int k) {
+
+        return 0;
+    }
+}
