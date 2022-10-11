97c50474317cd52d4adeda41446312621057ce77
张晋铭 <1574768045@qq.com>
2022-10-11 11:51:37 +0800

    最大值减去最小值小于或等于num的子数组数量

--- algorithms/src/com/sameen/study/submit/Main.java
+++ algorithms/src/com/sameen/study/submit/Main.java
@@ -1,21 +1,17 @@
-package com.sameen.study.leetcode;
+package com.sameen.study.submit;
 
-import com.sameen.study.submit.Main;
+import com.sameen.study.leetcode.StackAndQueueCode;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 
 /**
- * @author: zhangjinming on 2022/9/23
+ * @author: zhangjinming on 2022/10/11
  * @description:
  */
-public class StackAndQueueCode {
+public class Main {
 
     static class Reader {
         final private int BUFFER_SIZE = 1 << 16;
@@ -141,113 +137,9 @@ public class StackAndQueueCode {
     }
 
     public static void main(String[] args) throws IOException {
-        List<int[]> res = getNearLessNoRepeat(new int[]{3, 4, 1, 5, 6, 2, 7});
-        List<int[]> list = getNearLessRepeat(new int[]{3, 1, 3, 4, 3, 5, 3, 2, 2});
-        System.out.println(list);
         getNum();
     }
 
-    /**
-     * @param nums
-     * @param k
-     * @return
-     */
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums.length < k) {
-            return new int[0];
-        }
-        int[] res = new int[nums.length - k + 1];
-        Deque<Integer> deque = new LinkedList<>();
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i >= k && nums[i - k] == deque.peekFirst()) {
-                deque.pollFirst();
-            }
-            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
-                deque.pollLast();
-            }
-            deque.addLast(nums[i]);
-            if (i >= k - 1) {
-                res[index++] = deque.peekFirst();
-            }
-        }
-        return res;
-    }
-
-    /**
-     * 给定一个不含有重复值的数组arr，找到每一个i位置左边和右边离i位置最近且值比arr[i]小的位置。返回所有位置相应的信息。
-     * example: 3 4 1 5 6 2 7
-     *
-     * @param arr
-     * @return
-     */
-    public static List<int[]> getNearLessNoRepeat(int[] arr) {
-        Deque<Integer> leftStack = new ArrayDeque<>();
-        List<int[]> res = new ArrayList<>(arr.length);
-        for (int i = 0; i < arr.length; i++) {
-            res.add(new int[]{-1, -1});
-        }
-        for (int i = 0; i < arr.length; i++) {
-            while (!leftStack.isEmpty() && arr[i] < arr[leftStack.peekLast()]) {
-                int[] tmp = res.get(leftStack.pollLast());
-                tmp[1] = i;
-                tmp[0] = leftStack.isEmpty() ? -1 : leftStack.peekLast();
-            }
-            leftStack.addLast(i);
-        }
-        while (!leftStack.isEmpty()) {
-            int[] tmp = res.get(leftStack.pollLast());
-            tmp[0] = leftStack.isEmpty() ? -1 : leftStack.peekLast();
-        }
-        return res;
-    }
-
-    /**
-     * 含有重复值的数组arr
-     * example:{3，1，3，4，3，5，3，2，2}
-     *
-     * @param arr
-     * @return
-     */
-    public static List<int[]> getNearLessRepeat(int[] arr) {
-        Deque<List<Integer>> stack = new ArrayDeque<>();
-        List<int[]> res = new ArrayList<>(arr.length);
-        for (int i = 0; i < arr.length; i++) {
-            res.add(new int[]{-1, -1});
-        }
-        for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && arr[i] < arr[stack.peekLast().get(stack.peekLast().size() - 1)]) {
-                List<Integer> pollLast = stack.pollLast();
-                int leftLessIndex = stack.isEmpty() ? -1 : stack.peekLast().get(stack.peekLast().size() - 1);
-                for (Integer integer : pollLast) {
-                    int[] tmp = res.get(integer);
-                    tmp[1] = i;
-                    tmp[0] = leftLessIndex;
-                }
-            }
-            if (!stack.isEmpty() && arr[i] == arr[stack.peekLast().get(0)]) {
-                stack.peekLast().add(i);
-            } else {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(i);
-                stack.addLast(list);
-            }
-        }
-        while (!stack.isEmpty()) {
-            List<Integer> pollLast = stack.pollLast();
-            int leftLessIndex = stack.isEmpty() ? -1 : stack.peekLast().get(stack.peekLast().size() - 1);
-            for (Integer integer : pollLast) {
-                int[] tmp = res.get(integer);
-                tmp[0] = leftLessIndex;
-            }
-        }
-        return res;
-    }
-
-    /**
-     * https://www.nowcoder.com/practice/5fe02eb175974e18b9a546812a17428e?tpId=101&tqId=33086&rp=1&ru=/exam/oj/ta&qru=/exam/oj/ta&sourceUrl=%2Fexam%2Foj%2Fta%3Fpage%3D1%26pageSize%3D50%26search%3D%25E6%259C%2580%25E5%25A4%25A7%25E5%2580%25BC%26tpId%3D101%26type%3D101&difficulty=undefined&judgeStatus=undefined&tags=&title=%E6%9C%80%E5%A4%A7%E5%80%BC
-     * 最大值减去最小值小于或等于num的子数组数量
-     */
     public static void getNum() throws IOException {
         Reader reader = new Reader();
         int n = reader.nextInt();
@@ -291,6 +183,4 @@ public class StackAndQueueCode {
         }
         System.out.println(result);
     }
-
-
 }
