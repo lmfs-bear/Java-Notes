ef708af7ce2ad8e282c5496a4b1c2aab333fe56f
张晋铭 <1574768045@qq.com>
2022-10-13 11:23:30 +0800

    LinkedListCode:删除链表的倒数第 n 个结点 单链表

--- algorithms/src/com/sameen/study/leetcode/LinkedListCode.java
+++ algorithms/src/com/sameen/study/leetcode/LinkedListCode.java
@@ -7,7 +7,35 @@ package com.sameen.study.leetcode;
 public class LinkedListCode {
 
     /**
-     * 打印两个有序链表的公共部分
+     * 删除链表的倒数第 n 个结点 单链表
      */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode next = head, cur = head;
+        while (n-- > 0) {
+            if (cur == null) {
+                return head;
+            }
+            cur = cur.next;
+        }
+        if (cur == null) {
+            return head.next;
+        }
+        while (cur.next != null) {
+            cur = cur.next;
+            next = next.next;
+        }
+        next.next = next.next.next;
+        return head;
+    }
 
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
