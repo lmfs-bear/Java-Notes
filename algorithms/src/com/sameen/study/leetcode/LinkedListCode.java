f99352f50cf00d9c9daee7d91ee73e80fce38ac0
McGrady2002 <1574768045@qq.com>
2022-10-16 17:59:15 +0800

    LinkedListCode:isPalindrome

--- algorithms/src/com/sameen/study/leetcode/LinkedListCode.java
+++ algorithms/src/com/sameen/study/leetcode/LinkedListCode.java
@@ -8,6 +8,7 @@ public class LinkedListCode {
 
     /**
      * 删除链表的倒数第 n 个结点 单链表
+     * ············································
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode next = head, cur = head;
@@ -28,6 +29,25 @@ public class LinkedListCode {
         return head;
     }
 
+
+    /**
+     * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+     * 约瑟夫环
+     */
+    public int lastRemaining(int n, int m) {
+        return n == 1 ? 0 : (m + lastRemaining(n - 1, m)) % n;
+    }
+
+    /**
+     * 回文链表
+     * 用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
 }
 
 class ListNode {
